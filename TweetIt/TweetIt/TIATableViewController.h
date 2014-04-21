5d01047997d60933197ee9e152743a1d70099430
Jon Fox <foxjon123@gmail.com>
2014-04-21 10:19:30 -0400

    New Twitter App

---
+++ TweetIt/TweetIt/TIATableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TIATableViewController.h
+//  TweetIt
+//
+//  Created by Jonathan Fox on 4/21/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIATableViewController : UITableViewController
+
+@end
---
+++ TweetIt/TweetIt/TIAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TIAViewController.h
+//  TweetIt
+//
+//  Created by Jonathan Fox on 4/21/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIAViewController : UIViewController
+
+@end
