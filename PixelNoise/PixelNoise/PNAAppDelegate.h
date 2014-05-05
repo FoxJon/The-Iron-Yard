9a01623ac9919f9f86d6089893a0960befc0f084
Jon Fox <foxjon123@gmail.com>
2014-05-05 09:19:23 -0400

    NewApp

---
+++ PixelNoise/PixelNoise/PNAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PNAAppDelegate.h
+//  PixelNoise
+//
+//  Created by Jonathan Fox on 5/5/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PixelNoise/PixelNoise/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PixelNoise
+//
+//  Created by Jonathan Fox on 5/5/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PNAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PNAAppDelegate class]));
+    }
+}
