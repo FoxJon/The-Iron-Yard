7ee000afcab38b7dcbb13372f59c0fa81deeba8c
Jon Fox <foxjon123@gmail.com>
2014-05-06 00:09:16 -0400

    water drip with splash

--- PixelNoise/PNAWorldController.m
+++ PixelNoise/PNAWorldController.m
@@ -36,20 +36,35 @@
         self.squares = [@[]mutableCopy];
         self.smallSquares = [@[]mutableCopy];
         sounds = [[PNAPixelSounds alloc]init];
-
+        UIView *water = [[UIView alloc] initWithFrame:CGRectMake(0, 395, 320, 200)];
+        water.backgroundColor = [UIColor blueColor];
+        [self.view addSubview:water];
     }
     return self;
 }
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-
     UITouch *myTouch =[[event allTouches] anyObject];
     CGPoint location = [myTouch locationInView:self.view];
-    
     square = [[UIView alloc] initWithFrame:CGRectMake(location.x, location.y, 10, 10)];
-    square.backgroundColor = [UIColor whiteColor];
+    [self createSquare];
+}
+
+//-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+//{
+//    UITouch *myTouch =[[event allTouches] anyObject];
+//    CGPoint location = [myTouch locationInView:self.view];
+//    square = [[UIView alloc] initWithFrame:CGRectMake(location.x, location.y, 10, 10)];
+//    [self createSquare];
+//}
+
+-(void)createSquare
+{
+    
+    square.backgroundColor = [UIColor blueColor];
     [self.view addSubview:square];
- //   [self.squares addObject:square];
+//    [self.squares addObject:square];
 
     // Do any additional setup after loading the view.
     
@@ -60,11 +75,17 @@
     
     self.collision = [[UICollisionBehavior alloc]initWithItems:@[square]];
     self.collision.collisionDelegate = self;
-    self.collision.translatesReferenceBoundsIntoBoundary = YES;
+ //   self.collision.translatesReferenceBoundsIntoBoundary = YES;
     
-    self.collision.collisionMode = UICollisionBehaviorModeBoundaries;
+ //   self.collision.collisionMode = UICollisionBehaviorModeBoundaries;
     
     [self.animator addBehavior:self.collision];
+    
+    [self.collision addBoundaryWithIdentifier:@"floor"fromPoint:CGPointMake(0, 400) toPoint:CGPointMake(320, 400)];
+    
+//    UIDynamicItemBehavior* itemBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:@[square]];
+//    itemBehaviour.elasticity = 0.5;
+//    [self.animator addBehavior:itemBehaviour];
 }
 
 - (void)viewDidLoad
@@ -80,85 +101,77 @@
 
 -(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
 {
-     [sounds playSoundWithName:@"blip_click"];
-      NSLog(@"FLOOR");
-    
+    if ([(NSString *)identifier isEqualToString:@"floor"])
+    {
+        [sounds playSoundWithName:@"drip"];
+//
     [self.collision removeItem:square];
     [square removeFromSuperview];
-
+//
     CGRect frame = square.frame;
-
+//
         UIView *smallSquare1 = [[UIView alloc] initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 8, 8)];
-        smallSquare1.backgroundColor = [UIColor whiteColor];
+        smallSquare1.backgroundColor = [UIColor blueColor];
         [self.view addSubview:smallSquare1];
 //        [self.smallSquares addObject:smallSquare];
         self.gravity = [[UIGravityBehavior alloc] initWithItems:@[smallSquare1]];
         [self.animator addBehavior:self.gravity];
         self.pusher = [[UIPushBehavior alloc] initWithItems:@[smallSquare1] mode: UIPushBehaviorModeInstantaneous];
-        self.pusher.pushDirection = CGVectorMake(-0.002, -0.01);  // + is down, - is up. object mass makes diff
+        self.pusher.pushDirection = CGVectorMake(-0.002, -0.012);  // + is down, - is up. object mass makes diff
         [self.animator addBehavior:self.pusher];
-    
+
     UIView *smallSquare2 = [[UIView alloc] initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 8, 8)];
-    smallSquare2.backgroundColor = [UIColor whiteColor];
+    smallSquare2.backgroundColor = [UIColor blueColor];
     [self.view addSubview:smallSquare2];
     //        [self.smallSquares addObject:smallSquare];
     self.gravity = [[UIGravityBehavior alloc] initWithItems:@[smallSquare2]];
     [self.animator addBehavior:self.gravity];
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[smallSquare2] mode: UIPushBehaviorModeInstantaneous];
-    self.pusher.pushDirection = CGVectorMake(-0.004, -0.01);  // + is down, - is up. object mass makes diff
+    self.pusher.pushDirection = CGVectorMake(-0.005, -0.013);  // + is down, - is up. object mass makes diff
     [self.animator addBehavior:self.pusher];
-    
+
     UIView *smallSquare3 = [[UIView alloc] initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 8, 8)];
-    smallSquare3.backgroundColor = [UIColor whiteColor];
+    smallSquare3.backgroundColor = [UIColor blueColor];
     [self.view addSubview:smallSquare3];
     //        [self.smallSquares addObject:smallSquare];
     self.gravity = [[UIGravityBehavior alloc] initWithItems:@[smallSquare3]];
     [self.animator addBehavior:self.gravity];
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[smallSquare3] mode: UIPushBehaviorModeInstantaneous];
-    self.pusher.pushDirection = CGVectorMake(-0.009, -0.01);  // + is down, - is up. object mass makes diff
+    self.pusher.pushDirection = CGVectorMake(-0.009, -0.011);  // + is down, - is up. object mass makes diff
     [self.animator addBehavior:self.pusher];
-    
+
     UIView *smallSquare4 = [[UIView alloc] initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 8, 8)];
-    smallSquare4.backgroundColor = [UIColor whiteColor];
+    smallSquare4.backgroundColor = [UIColor blueColor];
     [self.view addSubview:smallSquare4];
     //        [self.smallSquares addObject:smallSquare];
     self.gravity = [[UIGravityBehavior alloc] initWithItems:@[smallSquare4]];
     [self.animator addBehavior:self.gravity];
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[smallSquare4] mode: UIPushBehaviorModeInstantaneous];
-    self.pusher.pushDirection = CGVectorMake(0.002, -0.01);  // + is down, - is up. object mass makes diff
+    self.pusher.pushDirection = CGVectorMake(0.002, -0.012);  // + is down, - is up. object mass makes diff
     [self.animator addBehavior:self.pusher];
     
     UIView *smallSquare5 = [[UIView alloc] initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 8, 8)];
-    smallSquare5.backgroundColor = [UIColor whiteColor];
+    smallSquare5.backgroundColor = [UIColor blueColor];
     [self.view addSubview:smallSquare5];
     //        [self.smallSquares addObject:smallSquare];
     self.gravity = [[UIGravityBehavior alloc] initWithItems:@[smallSquare5]];
     [self.animator addBehavior:self.gravity];
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[smallSquare5] mode: UIPushBehaviorModeInstantaneous];
-    self.pusher.pushDirection = CGVectorMake(0.004, -0.01);  // + is down, - is up. object mass makes diff
+    self.pusher.pushDirection = CGVectorMake(0.005, -0.013);  // + is down, - is up. object mass makes diff
     [self.animator addBehavior:self.pusher];
     
     UIView *smallSquare6 = [[UIView alloc] initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 8, 8)];
-    smallSquare6.backgroundColor = [UIColor whiteColor];
+    smallSquare6.backgroundColor = [UIColor blueColor];
     [self.view addSubview:smallSquare6];
     //        [self.smallSquares addObject:smallSquare];
     self.gravity = [[UIGravityBehavior alloc] initWithItems:@[smallSquare6]];
     [self.animator addBehavior:self.gravity];
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[smallSquare6] mode: UIPushBehaviorModeInstantaneous];
-    self.pusher.pushDirection = CGVectorMake(0.009, -0.01);  // + is down, - is up. object mass makes diff
+    self.pusher.pushDirection = CGVectorMake(0.009, -0.011);  // + is down, - is up. object mass makes diff
     [self.animator addBehavior:self.pusher];
 
-    
+    }
 }
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
