04a29021331c55cd1d98c0f2563c96e05beb8481
Jon Fox <foxjon123@gmail.com>
2014-05-14 13:36:43 -0400

    Ripple Animation

---
+++ DigitalPond/DPAPondViewController.h
@@ -0,0 +1,13 @@
+//
+//  DPAPondViewController.h
+//  DigitalPond
+//
+//  Created by Jonathan Fox on 5/14/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAPondViewController : UIViewController
+
+@end
---
+++ DigitalPond/DPARipple.h
@@ -0,0 +1,16 @@
+//
+//  DPARipple.h
+//  DigitalPond
+//
+//  Created by Jonathan Fox on 5/14/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPARipple : UIView
+
+@property (nonatomic)float rippleCount;
+@property (nonatomic)float rippleLifeTime;
+
+@end
