a8e3a8871bbb6d697b7b8e17c445a469383c1929
Jon Fox <foxjon123@gmail.com>
2014-05-25 14:23:53 -0400

    Core  Motion example

---
+++ CoreMotion/CMAViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMAViewController.h
+//  CoreMotion
+//
+//  Created by Jonathan Fox on 5/22/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAViewController : UIViewController
+
+@end
---
+++ CoreMotion/CoreMotion/CMAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMAAppDelegate.h
+//  CoreMotion
+//
+//  Created by Jonathan Fox on 5/22/14.
+//  Copyright (c) 2014 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
